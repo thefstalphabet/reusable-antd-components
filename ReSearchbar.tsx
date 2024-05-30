5e736a81731b235e02aa030f7bdf5992e402ad2f
Akash patel <akashpatel10000@gmail.com>
2024-05-30 12:48:06 +0530

    Update: new research bar component

---
+++ ReSearchbar.tsx
@@ -0,0 +1,37 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Input } from "antd";
+import { useState } from "react";
+
+export default function ReSearchbar(props: {
+  className?: string;
+  placeholder?: string;
+  size: "small" | "large";
+  onSubmit: (searchTerm: string) => void;
+}) {
+  const { className, placeholder, size, onSubmit } = props;
+
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const handleEnterHits = (e: any) => {
+    if (e?.key === "Enter") {
+      onSubmit(searchTerm);
+    }
+  };
+  return (
+    <Input
+      size={size}
+      placeholder={placeholder}
+      className={className}
+      onChange={(e) => {
+        setSearchTerm(e?.target?.value);
+      }}
+      onPressEnter={handleEnterHits}
+      prefix={
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          style={{ marginRight: "5px" }}
+        />
+      }
+    />
+  );
+}
